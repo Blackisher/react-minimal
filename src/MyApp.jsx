76e61c7491979cac4d75c4550b2be34767974d7b
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-10-15 23:11:22 +0200

    LAB-1 Task 3 "Exact same as above but with random numbers from 1 to 25. The method should be named generateRandomArray."

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -8,11 +8,16 @@ const MyApp = () => (
     <p>Bundle size: 57 bytes (first 40,7 KB), Load time of the bundle: 48 ms (first 211 ms), Last commit SHA1: 0a647f01730022190f9588c241a93900569203a5</p>
     <AppTitle />
     <h2>GenerateArray for 20 ==> {generateArray(20)}</h2>
+    <h2>GenerateRandomArray for 5 ==> {generateRandomArray(5)}</h2>
   </>
 )
 
 const generateArray = (n) => {
     return "[" + (Array.from(Array(n).keys(), (v) => v+1)) +"]";
+}
+
+const generateRandomArray = (n) => {
+    return "[" + (Array.from(Array(n).keys(), (v) => Math.floor(1+Math.random()*25))) +"]";
 }
 
 export default MyApp
