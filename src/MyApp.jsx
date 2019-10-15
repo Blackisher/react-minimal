26c2562f162ceaa04729137ee6903eb17963434c
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-10-15 23:59:33 +0200

    LAB-1 Task 7 "init"

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -74,6 +74,16 @@ const MyApp = () => (
     <h2>Task 5.Declare const containing array: [2, 5, 8, 10]. Create method that returns an array with the square root values of each number.</h2>
     <p>{printSquareRoots(myArrayForTask5).toString()}</p>
     <h3>Task 6. Download and copy the JSON provided here into a variable called “data” in the App.js file.</h3>
+    <h3>Task 7. Add 3 buttons to the page called respectively “All students”, “Sort students”, “Old students”.</h3>
+        <h4>When clicked the buttons should display respectively.</h4>
+        <h4>a) The list of all students names</h4>
+        <h4>b) The list of all students names sorted alphabetically</h4>
+        <h4>c) The list of all students older than 20 but only from active teachers</h4>
+        <h4>For a, b c don’t use console log. Display data on the website.</h4>
+      <div>
+
+
+      </div>
 
   </>
 )
