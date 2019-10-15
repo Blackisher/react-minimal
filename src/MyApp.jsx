f5453527c185eee6e77aeaefc5df284e22359967
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-10-15 21:42:35 +0200

    LAB-1 “Production bundle analysis”

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React blackisher</h1>
+  <>
+    <h1>Minimal React blackisher</h1>
+    <p>Bundle size: 57 bytes (first 40,7 KB), Load time of the bundle: 48 ms (first 211 ms), Last commit SHA1: 0a647f01730022190f9588c241a93900569203a5</p>
+  </>
 )
 
 export default MyApp
