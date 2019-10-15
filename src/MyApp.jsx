8f69840c818583227d52d3ad312c63e95860d9ff
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-10-15 23:03:01 +0200

    LAB-1 Task 2 "In your App.js file create a function that will generate an array of integers from 1 to n (i.e. [1, 2, 3, … , n]). The prototype should be: const generateArray = (n) => … The method must be a one line (Google and Stack Overflow are your friends)."

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -7,7 +7,12 @@ const MyApp = () => (
     <h1>Minimal React blackisher</h1>
     <p>Bundle size: 57 bytes (first 40,7 KB), Load time of the bundle: 48 ms (first 211 ms), Last commit SHA1: 0a647f01730022190f9588c241a93900569203a5</p>
     <AppTitle />
+    <h2>GenerateArray for 20 ==> {generateArray(20)}</h2>
   </>
 )
+
+const generateArray = (n) => {
+    return "[" + (Array.from(Array(n).keys(), (v) => v+1)) +"]";
+}
 
 export default MyApp
