c917be94836ab78b5926d2802cf8c0a1b2b767ad
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-10-15 22:23:10 +0200

    LAB-1 Task 1 "Create a new component called AppTitle. It should be in it’s own *.js file. Inspire yourself from App.js to create the component. Import your new component into App.js and render it. "

---
+++ src/AppTitle.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppTitle = () => (
+  <>
+    <h2>Title Lab-1 added though old App.js_x</h2>
+  </>
+)
+
+export default AppTitle
--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import AppTitle from './AppTitle'
+import {render} from "react-dom";
 
 const MyApp = () => (
   <>
     <h1>Minimal React blackisher</h1>
     <p>Bundle size: 57 bytes (first 40,7 KB), Load time of the bundle: 48 ms (first 211 ms), Last commit SHA1: 0a647f01730022190f9588c241a93900569203a5</p>
+    <AppTitle />
   </>
 )
 
