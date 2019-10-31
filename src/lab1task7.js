b4e295d67d09f64df8802de5ebabd4256709e10e
Blackisher <36049543+Blackisher@users.noreply.github.com>
2019-10-31 08:07:30 +0100

    Lab2Ex1 done

--- src/lab1task7.js
+++ src/lab1task7.js
@@ -4,6 +4,9 @@ class SelectControl extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
+            a: 0,
+            b: 1,
+            c: 2,
             output: '',
             original: [
                 {
@@ -68,6 +71,8 @@ class SelectControl extends React.Component {
         this.handleClickAllStudentsName = this.handleClickAllStudentsName.bind(this);
         this.handleClickAllStudentsNameAlphabetically = this.handleClickAllStudentsNameAlphabetically.bind(this);
         this.handleClickOlderAndActiveTeachers = this.handleClickOlderAndActiveTeachers.bind(this);
+        this.handleOnChangeA = this.handleOnChangeA.bind(this);
+        this.handleOnChangeB = this.handleOnChangeB.bind(this);
 
     }
 
@@ -133,6 +138,44 @@ class SelectControl extends React.Component {
         )
     }
 
+    handleOnChangeA(ev) {
+        const re = /^[0-9\b]+$/;
+        if (re.test(ev.target.value)) {
+            this.setState({
+                a: ev.target.value
+            })
+            console.log("Value changed a: " + ev.target.value)
+        }
+
+    }
+    /* possible solution:
+    handleOnChangeB(ev) {
+        const re = /^[0-9\b]+$/;
+        if (re.test(ev.target.value)) {
+            this.setState({
+                b: ev.target.value
+            })
+            console.log("Value changed b: " + ev.target.value)
+        }
+    }
+    extended below:
+    */
+    handleOnChangeB(ev) {
+        const re = /^[0-9\b]+$/;
+        if (re.test(ev.target.value)) {
+            if (ev.target.name == "b") {
+                this.setState({
+                    b: ev.target.value
+                })
+            } else if (ev.target.name == "c") {
+                this.setState({
+                    c: ev.target.value
+                })
+            }
+            console.log("Value changed " + ev.target.name + ": " + ev.target.value)
+        }
+    }
+
     render() {
         return (
             <div>
@@ -143,6 +186,22 @@ class SelectControl extends React.Component {
                        value="The list of all students names sorted alphabetically"/>
                 <input type="button" onClick={this.handleClickOlderAndActiveTeachers}
                        value="The list of all students older than 20 but only from active teachers"/>
+
+                <h1>LAB 2</h1>
+                <h4>Lab2Ex1 Create 2 input component allowing to input only numbers. Let’s call the numbers a and b. You will need to
+                    keep the value of the inputs in the state of your app. Changing the value in the input should work and
+                    display in the console “Value changed a: 12” (“a” can be “a” or “b” and “12” is the new value of “a” in the
+                    example).</h4>
+                a = {this.state.output}. Component for a ==> <input type="number" onChange={this.handleOnChangeA} value={this.state.a}/>
+                <br/>
+                b = {this.state.output}. Component for b ==> <input type="number" onChange={this.handleOnChangeB} value={this.state.b} name={"b"}/>
+                <br/>
+                In case there should be only one handler... connecting b and c to the same handler - discover by name
+                <br/>
+                c = {this.state.output}. Component for c ==> <input type="number" onChange={this.handleOnChangeB} value={this.state.c} name={"c"}/>
+                <br/>
+                In addition those fields (a,b,c) don't accept "e" and dot.
+                <br/>
 
             </div>
         );
